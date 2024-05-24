0298c68154061931a9fc00795eb4d96dd01d6d17
David Pimentel <david.pimentel65@gmail.com>
2024-05-24 12:32:37 -0400

    case study page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,4 @@
 import { defineConfig } from "astro/config";
-import rehypeClassNames from "rehype-class-names";
 
 // https://astro.build/config
-export default defineConfig({
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeClassNames,
-        {
-          h1: "h1",
-          h2: "h2",
-          h3: "h3",
-          h4: "h4",
-          h5: "h5",
-          h6: "h6",
-          p: "p",
-          ul: "ul",
-          ol: "ol",
-          li: "li",
-        },
-      ],
-    ],
-  },
-});
+export default defineConfig({});
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,11 @@ import { z, defineCollection } from "astro:content";
 const casesCollection = defineCollection({
   type: "content",
   schema: z.object({
-    name: z.string(),
+    title: z.string(),
     tags: z.array(z.string()),
+    jobs: z.array(z.string()),
     thumbnail: z.string(),
+    images: z.array(z.string()),
   }),
 });
 export const collections = {
