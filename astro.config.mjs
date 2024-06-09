2270b1c8f086211b10bcd3e59dac1da0ddabbc3c
David Pimentel <david.pimentel65@gmail.com>
2024-06-09 18:07:37 -0400

    removed readme, fixed links for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // site: 'https://nmformica.com',
+  site: "https://nmforms.github.io/",
+  base: "brooklyncreativeco.com",
+});
