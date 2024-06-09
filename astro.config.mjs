4b9205f6d13680c7bdab426fab1a1cd2cc760629
David Pimentel <david.pimentel65@gmail.com>
2024-06-09 18:12:17 -0400

    trying to fix links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   // site: 'https://nmformica.com',
-  site: "https://nmforms.github.io/",
-  base: "brooklyncreativeco.com",
+  site: "https://nmforms.github.io",
+  base: "/brooklyncreativeco.com",
 });
